97fad41eaeaa23c27c1c2f1afb16a88f5b32384f
MorschaVG <morschavongrumbkow@gmail.com>
2025-06-10 21:43:41 +0200

    Nieuw Datacamp mapje

---
+++ For loops.py
@@ -0,0 +1,24 @@
+# looping through dictionaries
+
+# products_dict = {"Henk":10.00, "Piet":20.00,
+#                  "Willem": 30.00, "Kees": 40.00}
+#
+# #loop through keys and values
+#
+# for val in products_dict.values():
+#     print(val)
+#
+# for i in range(1, 6):
+#     print(i)
+
+# Building a counter
+#no visits yet
+visits = 0
+#loop through numbers 1-10
+for i in range(1, 11):
+    #add one to visits during each iteration
+    visits += 1 #same as visits = visits + 1
+
+print(visits)
+
+
